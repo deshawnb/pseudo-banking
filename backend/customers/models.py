16fd3265f939b965b96ee7446cb25dafdc230e2a
Deshawn Brown <brownd726@gmail.com>
2022-05-26 15:32:08 -0500

    feat: Built the Customer model for the customers app

--- backend/customers/models.py
+++ backend/customers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+
+class Customer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    street_address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=100)
+    membership_history = models.TimeField()
