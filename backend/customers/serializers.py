2a6a577b5ae814bf2a2a71d158cc39b45fe1dd1b
Deshawn Brown <brownd726@gmail.com>
2022-05-27 10:05:36 -0500

    fix: fixed typo in serializer

--- backend/customers/serializers.py
+++ backend/customers/serializers.py
@@ -4,5 +4,5 @@ from .models import Customer
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        feilds = ['id', 'user', 'first_name', 'last_name', 'street_address', 'city', 'state', 'zip_code', 'membership_history']
+        fields = ['id', 'user', 'first_name', 'last_name', 'street_address', 'city', 'state', 'zip_code', 'membership_history']
         depth = 1
