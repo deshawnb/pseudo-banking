750eb5cfed137abafae2cc6a3bc3b5ab9245503c
Deshawn Brown <brownd726@gmail.com>
2022-05-31 11:08:18 -0500

    feat: Built get request for apps to get all objects related to apps

--- backend/deposits/views.py
+++ backend/deposits/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated,AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import DepositSerializer
+from .models import Deposit
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all(request):
+    deposit = Deposit.objects.all()
+    serializer = DepositSerializer(deposit , many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
