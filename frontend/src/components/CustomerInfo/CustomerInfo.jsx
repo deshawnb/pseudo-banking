f50ab01bc587b64b9097bad1daf4ea0c3070df41
Deshawn Brown <brownd726@gmail.com>
2022-06-01 11:06:14 -0500

    feat: Built customer info component to display user info

---
+++ frontend/src/components/CustomerInfo/CustomerInfo.jsx
@@ -0,0 +1,23 @@
+
+
+const CustomerInfo = (props) => {
+    return (
+        <div>
+            {props.parentCustomers.map((customer) => {
+                return (
+                    <div>
+                        <p>First Name: {customer.first_name}</p>
+                        <p>Last Name: {customer.last_name}</p>
+                        <p>Address: {customer.street_address}</p>
+                        <p>City: {customer.city}</p>
+                        <p>State: {customer.state}</p>
+                        <p>Zip Code: {customer.zip_code}</p>
+                        <p>Memebership History: {customer.membership_history}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
+export default CustomerInfo
