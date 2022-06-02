edd3fc99875a0181abd2eed31000e40a8fc3d9b8
Deshawn Brown <brownd726@gmail.com>
2022-06-02 10:13:18 -0500

    improvement: Updated models and serializers of banking accounts app

--- backend/banking_accounts/models.py
+++ backend/banking_accounts/models.py
@@ -5,7 +5,7 @@ from authentication.models import User
 
 # Create your models here.
 class BankingAccount(models.Model):
-    users = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     account_type = models.ForeignKey(AccountType, on_delete=models.CASCADE)
     balance = models.IntegerField()
     budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
--- backend/banking_accounts/serializers.py
+++ backend/banking_accounts/serializers.py
@@ -4,5 +4,4 @@ from .models import BankingAccount
 class BankingAccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = BankingAccount
-        fields = ['id', 'users', 'account_type', 'balance', 'budget']
-        depth = 1
+        fields = ['id', 'user', 'account_type', 'balance', 'budget']
