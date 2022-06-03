0702d3f273f8817f8a71123ac15c7467cf2bf02e
Deshawn Brown <brownd726@gmail.com>
2022-06-03 16:27:10 -0500

    refactor: updated bank account components

--- frontend/src/components/BankAccount/BankAccount.jsx
+++ frontend/src/components/BankAccount/BankAccount.jsx
@@ -3,10 +3,12 @@
 const BankAccount = (props) => {
     return (
         <div>
+            <p>owner: {props.user.username}</p>
+            <p>Account Name: {props.account_name}</p>
             <p>Account number: {props.id}</p>
-            <p>Account type: {props.account_type}</p>
-            <p>Balance: {props.balance}</p>
-            <p>Budget: {props.budget}</p>
+            <p>Account type: {props.account_type.type}</p>
+            <p>Balance: {props.balance}$</p>
+            <p>Budget Limit: {props.budget.budget_limit}</p>
         </div>
     );
 }
--- frontend/src/components/BankAccountList/BankAccountList.jsx
+++ frontend/src/components/BankAccountList/BankAccountList.jsx
@@ -1,6 +1,5 @@
 import BankAccount from "../BankAccount/BankAccount";
 import { useNavigate, Link } from "react-router-dom";
-import BankAccountForm from "../BankAccountForm/BankAccountForm";
 
 const BankAccountList = (props) => {
     const navigate = useNavigate();
@@ -10,7 +9,7 @@ const BankAccountList = (props) => {
             {props.parentAccounts.map((account) => {
                 return (
                     <div>
-                        <BankAccount id={account.id}   account_type={account.account_type} balance={account.balance} budget={account.budget}/>
+                        <BankAccount id={account.id} user={account.user} account_name={account.account_name} account_type={account.account_type} balance={account.balance} budget={account.budget}/>
                         <button onClick={() => navigate(`/${account.id}`)}>Deposit</button>
                         <button onClick={() => navigate(`/${account.id}`)}>Withdraw</button>
                     </div>
