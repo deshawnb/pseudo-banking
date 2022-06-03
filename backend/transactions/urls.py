18889a4a889571c98ba575d7317ead8e0a6b69a3
Deshawn Brown <brownd726@gmail.com>
2022-06-03 11:14:32 -0500

    feat: Added authenticated api request for transactions app

--- backend/transactions/urls.py
+++ backend/transactions/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('all/', views.get_all)
+    path('all/', views.get_all),
+    path('', views.transaction_info),
+    path('<pk>/', views.transactions_by_detail)
 ]
--- backend/transactions/views.py
+++ backend/transactions/views.py
@@ -12,3 +12,35 @@ def get_all(request):
     transaction = Transaction.objects.all()
     serializer = TransactionSerializer(transaction , many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def transaction_info(request):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    if request.method == 'POST':
+        serializer = TransactionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        transaction = Transaction.objects.filter(user_id=request.user.id)
+        serializer = TransactionSerializer(transaction, many=True)
+        return Response(serializer.data)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated]) 
+def transactions_by_detail(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+    if request.method == "GET":
+        serializer = TransactionSerializer(transaction)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = TransactionSerializer(transaction, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        transaction.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
