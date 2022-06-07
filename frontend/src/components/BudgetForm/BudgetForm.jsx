54ad6d46561841b5a1a7564ba65a4f85acaa6f67
Deshawn Brown <brownd726@gmail.com>
2022-06-07 10:00:57 -0500

    feat: Built budget and transaction form components

---
+++ frontend/src/components/BudgetForm/BudgetForm.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+
+const BudgetForm = (props) => {
+    const [budget_name, setBudgetName] = useState('');
+    const [budget_limit, setBudgetLimit] = useState(0);
+    let user_id = props.user_id
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newBudget = {
+            user_id : user_id,
+            budget_name : budget_name,
+            budget_limit : budget_limit,
+        };
+        console.log(newBudget)
+        props.addNewBudgetProperty(newBudget)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>New Account name</label>
+            <input type='text' value={budget_name} onChange={(event) =>setBudgetName(event.target.value)}/>
+            <label>Budget Limit</label>
+            <input type='num' value={budget_limit} onChange={(event) =>setBudgetLimit(event.target.value)}/>
+            <button type="submit">Add New Budget</button>
+        </form>
+    )
+}
+
+export default BudgetForm
+
---
+++ frontend/src/components/TransactionForm/TransactionForm.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+
+const TransactionForm = (props) => {
+    const [transaction_name, setTransactionName] = useState('');
+    const [transaction_type_id, setTransactionTypeId] = useState(0);
+    const [amount_transferred, setAmountTransferred] = useState(0);
+    let user_id = props.user_id
+    let bank_account_id = props.bank_account_id
+    let transaction_types = props.transaction_types
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newTransaction = {
+            user_id : user_id,
+            bank_account_id : bank_account_id,
+            transaction_type_id : transaction_type_id,
+            amount_transferred : amount_transferred,
+            transaction_name : transaction_name,
+        };
+        console.log(newTransaction)
+        props.addNewBudgetProperty(newTransaction)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Transaction Name</label>
+            <input type='text' value={transaction_name} onChange={(event) =>setTransactionName(event.target.value)}/>
+            <select id={transaction_type_id} onChange={(event) =>setTransactionTypeId(event.target.value)}> 
+                <option value={0}>Select account type</option>
+                {transaction_types.map((transaction_type) => {
+                    return (
+                        <option value={`${transaction_type.id}`}>{`${transaction_type.type}`}</option>
+                    );
+                })}
+            </select>
+
+            <label>Amount Transferred</label>
+            <input type='int' value={amount_transferred} onChange={(event) =>setAmountTransferred(event.target.value)}/>
+            <button type="submit">Add New Budget</button>
+        </form>
+    )
+}
+
+export default TransactionForm
