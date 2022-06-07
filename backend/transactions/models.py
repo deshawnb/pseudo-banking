84556e7f7a87c2e40c7c7055edda83a9f8d5a1e4
Deshawn Brown <brownd726@gmail.com>
2022-06-07 09:45:10 -0500

    improvement: Added bank account foreign key to transaction model

--- backend/transactions/models.py
+++ backend/transactions/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from transaction_types.models import TransactionType
+from banking_accounts.models import BankingAccount
 from authentication.models import User
 
 # Create your models here.
 class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     transaction_type = models.ForeignKey(TransactionType, on_delete=models.CASCADE)
+    bank_account = models.ForeignKey(BankingAccount, on_delete=models.CASCADE)
     transaction_name = models.CharField(max_length=100)
     amount_transferred = models.IntegerField()
     time_of_transaction = models.DateTimeField(auto_now_add=True)
--- backend/transactions/serializers.py
+++ backend/transactions/serializers.py
@@ -4,8 +4,9 @@ from .models import Transaction
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
-        fields = ['id', 'user', 'user_id', 'transaction_type', 'transaction_type_id', 'transaction_name', 'amount_transferred', 'time_of_transaction']
+        fields = ['id', 'user', 'user_id', 'bank_account', 'bank_account_id', 'transaction_type', 'transaction_type_id', 'transaction_name', 'amount_transferred', 'time_of_transaction']
         depth = 1
 
     user_id = serializers.IntegerField(write_only=True)
+    bank_account_id = serializers.IntegerField(write_only=True)
     transaction_type_id = serializers.IntegerField(write_only=True)
