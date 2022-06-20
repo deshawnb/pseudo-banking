e3dd1f81a3a07cddae7894a8bb897d1f48c18384
Deshawn Brown <brownd726@gmail.com>
2022-06-20 09:32:35 -0500

    feat: Transactions now affect the balace of bank accounts based on the type of transaction.

--- frontend/src/components/TransactionForm/TransactionForm.jsx
+++ frontend/src/components/TransactionForm/TransactionForm.jsx
@@ -1,25 +1,70 @@
 import React, { useState } from "react";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
 
 
 const TransactionForm = (props) => {
+    const [user, token] = useAuth();
     const [transaction_name, setTransactionName] = useState('');
     const [transaction_type_id, setTransactionTypeId] = useState(0);
     const [amount_transferred, setAmountTransferred] = useState(0);
     let user_id = props.user_id
     let bank_account_id = props.bank_account_id
     let transaction_types = props.transaction_types
+    let account = props.account[0]
+    console.log(props.account)
+
+    const setBalance = async (balance) => {
+        try {
+          let response = await axios.put(`http://127.0.0.1:8000/api/banking_accounts/${bank_account_id}/`, {
+            user_id : account.user.id,
+            account_name : account.account_name,
+            account_type_id : account.account_type.id,
+            balance : balance,
+            budget_id : account.budget.id,
+          }, {
+            headers: {
+              Authorization: "Bearer " + token,
+            }, 
+          });
+          console.log(balance)
+        }
+         catch (error) {
+          console.log(error.message);
+        }
+    };
 
     function handleSubmit(event) {
         event.preventDefault();
-        let newTransaction = {
-            user_id : user_id,
-            bank_account_id : bank_account_id,
-            transaction_type_id : transaction_type_id,
-            amount_transferred : amount_transferred,
-            transaction_name : transaction_name,
-        };
-        console.log(newTransaction)
-        props.addNewBudgetProperty(newTransaction)
+        let balance = parseInt(account.balance)
+        if(transaction_type_id == 1){
+            let newTransaction = {
+                user_id : user_id,
+                bank_account_id : bank_account_id,
+                transaction_type_id : transaction_type_id,
+                amount_transferred : parseInt(`-${amount_transferred}`),
+                transaction_name : transaction_name,
+            };
+            let newBalance = (parseInt(balance) - parseInt(amount_transferred));
+            setBalance(newBalance)
+            console.log(balance)
+            props.addNewBudgetProperty(newTransaction)
+            window.location.reload(false);
+        }
+        else if(transaction_type_id == 2){
+            let newTransaction = {
+                user_id : user_id,
+                bank_account_id : bank_account_id,
+                transaction_type_id : transaction_type_id,
+                amount_transferred : parseInt(amount_transferred),
+                transaction_name : transaction_name,
+            };
+            let newBalance =  (parseInt(balance) + parseInt(amount_transferred));
+            setBalance(newBalance)
+            console.log(balance)
+            props.addNewBudgetProperty(newTransaction)
+            window.location.reload(false);
+        }
     }
 
     return (
@@ -37,7 +82,9 @@ const TransactionForm = (props) => {
 
             <label>Amount Transferred</label>
             <input type='int' value={amount_transferred} onChange={(event) =>setAmountTransferred(event.target.value)}/>
-            <button type="submit">Add New Budget</button>
+
+                <button type="submit" >make Transfer</button>
+
         </form>
     )
 }
